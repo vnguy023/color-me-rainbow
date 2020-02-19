939be883253d2991550979e1899d8bc3d491f3bb
Vincent Nguyen <vincentnguyen88@hotmail.com>
2020-02-19 13:52:41 -0800

    we printout colors used in an image, Images are passed instead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import getopt, glob, sys
+import getopt, glob, os, sys
+
+from PIL import Image
 
 import process
 
@@ -12,11 +14,30 @@ def main(imageFolder, maskFile, colorMappingFile, outputFolder):
 
     filesProcessed = 0
 
+    try:
+        os.makedirs(outputFolder)
+    except OSError:
+        print ("Folder already exists. Adding/overwriting data")
+    else:
+        print ("Created Output Folder")
+
     # Pretty sure this doesn't search inside folders too
     for ext in SUPPORTED_IMAGE_EXTENSIONS:
         for fileName in glob.glob(imageFolder + "*." + ext):
-            if process.processImage(fileName):
-                filesProcessed += 1
+            filesProcessed += 1
+            
+            baseFileName = os.path.splitext(os.path.basename(fileName))[0]
+            image = Image.open(fileName)
+            hsvImage = image.convert("HSV")
+
+            print ("Unique Colors (HSV):")
+            print(process.getColors(hsvImage))
+            process.processImageHSV(hsvImage)
+
+            print ("Saving: "+ baseFileName + ".png")
+            savePath = outputFolder + baseFileName + ".png"
+
+            image.save(savePath)
 
     print ("Files Processed: " + str(filesProcessed))
     exit()
@@ -25,9 +46,9 @@ numArgs = len(sys.argv) - 1
 
 # This parses arugments to make sure it's valid
 if numArgs == 2:
-    main(sys.argv[1], sys.argv[2], "./colorProfiles/edg32.colorProfile", "./output")
+    main(sys.argv[1], sys.argv[2], "./colorProfiles/edg32.colorProfile", "./output/")
 elif numArgs == 0:
-    main("./images/", "mask.mask", "./colorProfiles/edg32.colorProfile", "./output")
+    main("./images/", "mask.mask", "./colorProfiles/edg32.colorProfile", "./output/")
 else:
     print("Format <imageFolder> <maskFile> <colorProfile> <outputFolder>")
     exit()
--- process.py
+++ process.py
@@ -1,9 +1,27 @@
 from PIL import Image
 
-def processImage(imageFileName):
-    print ("processing file: " + imageFileName)
+def processImageHSV(image):
+    result = Image.new("HSV",image.size)
+    print (image.format, image.size, "HSV")
 
-    image = Image.open(imageFileName)
-    print (image.format, image.size, image.mode)
+    for x in range(image.size[0]):
+        for y in range(image.size[1]):
+            pixel = image.getpixel((x,y))
+            # if it doesn't map to any known values
+            result.putpixel( (x,y), pixel)
 
-    return True
+    return result
+
+def getColors(image):
+    print ("GetColorsMode")
+    print(image.mode)
+
+    myDict = {}
+    for x in range(image.size[0]):
+        for y in range(image.size[1]):
+            myDict[image.getpixel((x,y))] = True
+
+    myList = []
+    for key in myDict:
+        myList.append(key)
+    return myList 
