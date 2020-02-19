f570449d77a31a71b2070aca5ef88b6a51e75f67
Vincent Nguyen <vincentnguyen88@hotmail.com>
2020-02-19 00:35:45 -0800

    handle arguements and read filenames

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hello World")
+import getopt, glob, sys
+
+from PIL import Image
+
+SUPPORTED_IMAGE_EXTENSIONS = ["png"]
+
+def processImage(imageFileName):
+    print ("processing file: " + imageFileName)
+
+    image = Image.open(imageFileName)
+    print (image.format, image.size, image.mode)    
+
+def main(imageFolder, maskFile, colorMappingFile, outputFolder):
+    print("ImageFolder: " + imageFolder)
+    print("MaskFile: " + maskFile)
+    print("Color Profile" + colorMappingFile)
+    print("Output Folder: " + outputFolder)
+
+    filesProcessed = 0
+
+    for ext in SUPPORTED_IMAGE_EXTENSIONS:
+        for fileName in glob.glob(imageFolder + "*." + ext):
+            filesProcessed += 1
+            processImage(fileName)
+
+    print ("Files Processed: " + str(filesProcessed))
+    exit()
+
+numArgs = len(sys.argv) - 1
+
+# This parses arugments to make sure it's valid
+if numArgs == 2:
+    main(sys.argv[1], sys.argv[2], "./colorProfiles/edg32.colorProfile", "./output")
+elif numArgs == 0:
+    main("./images/", "mask.mask", "./colorProfiles/edg32.colorProfile", "./output")
+else:
+    print("Format <imageFolder> <maskFile> <colorProfile> <outputFolder>")
+    exit()
+    
